b10dce48d63e2ffdaabeafeefd263e1488f46182
Calvin Hogg <146632082+nzfinaldrives1101@users.noreply.github.com>
2023-10-02 16:44:29 +1300

    update

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,81 @@
-var f1a = document.getElementById("f1a");
-var f1b = document.getElementById("f1b");
-var all = document.getElementsByClassName("faq-body");
-f1b.style.height = "0px";
+var f1 = document.getElementById("f1");
+var f2 = document.getElementById("f2");
+var f3 = document.getElementById("f3");
+var f4 = document.getElementById("f4");
+var f5 = document.getElementById("f5");
+var f6 = document.getElementById("f6");
+var f7 = document.getElementById("f7");
+var f8 = document.getElementById("f8");
+f1.style.height = "0px";
+f2.style.height = "0px";
+f3.style.height = "0px";
+f4.style.height = "0px";
+f5.style.height = "0px";
+f6.style.height = "0px";
+f7.style.height = "0px";
+f8.style.height = "0px";
 
 
 function toggle1() {
-	if (f1b.style.height == "0px") {
-		f1b.style.height = "auto";
+	if (f1.style.height == "0px") {
+		f1.style.height = "auto";
 	} else {
-		f1b.style.height = "0px";
+		f1.style.height = "0px";
+	}
+}
+
+function toggle2() {
+	if (f2.style.height == "0px") {
+		f2.style.height = "auto";
+	} else {
+		f2.style.height = "0px";
+	}
+}
+
+function toggle3() {
+	if (f3.style.height == "0px") {
+		f3.style.height = "auto";
+	} else {
+		f3.style.height = "0px";
+	}
+}
+
+function toggle4() {
+	if (f4.style.height == "0px") {
+		f4.style.height = "auto";
+	} else {
+		f4.style.height = "0px";
+	}
+}
+
+function toggle5() {
+	if (f5.style.height == "0px") {
+		f5.style.height = "auto";
+	} else {
+		f5.style.height = "0px";
+	}
+}
+
+function toggle6() {
+	if (f6.style.height == "0px") {
+		f6.style.height = "auto";
+	} else {
+		f6.style.height = "0px";
+	}
+}
+
+function toggle7() {
+	if (f7.style.height == "0px") {
+		f7.style.height = "auto";
+	} else {
+		f7.style.height = "0px";
+	}
+}
+
+function toggle8() {
+	if (f8.style.height == "0px") {
+		f8.style.height = "auto";
+	} else {
+		f8.style.height = "0px";
 	}
 }
