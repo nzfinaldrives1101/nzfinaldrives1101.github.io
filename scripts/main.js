a7a6e041e400b77aed325ba16d77045fc0fe6549
Calvin Hogg <146632082+nzfinaldrives1101@users.noreply.github.com>
2023-10-03 22:13:18 +1300

    update

--- scripts/main.js
+++ scripts/main.js
@@ -15,6 +15,59 @@ f6.style.height = "0px";
 f7.style.height = "0px";
 f8.style.height = "0px";
 
+search = document.getElementById("search");
+dropdown = document.getElementById("manufacturer");
+
+
+function resetProductModal() {
+	search.value = '';
+	dropdown.value = 'all';
+}
+
+function manufacturerSelected(value) {
+	result = document.getElementsByClassName("result");
+	for (index = 0; index < result.length; ++index) {
+		if (value == "airman") {
+	    	result[index].style.height ="0px";
+		}
+	}
+}
+
+$(document).ready(function(){
+	$("#search").on("input", function() {
+		var value = $(this).val().toLowerCase();
+		$("#results li").filter(function() {
+			$(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+		});
+	});
+});
+
+
+
+// function search() {
+//   // Declare variables
+//   var input, filter, table, tr, td, i, txtValue;
+//   input = document.getElementById("search");
+//   filter = input.value.toUpperCase();
+//   table = document.getElementById("results");
+//   tr = table.getElementsByTagName("tr");
+//
+//   // Loop through all table rows, and hide those who don't match the search query
+//   for (i = 0; i < tr.length; i++) {
+//     td = tr[i].getElementsByTagName("td")[0];
+//     if (td) {
+//       txtValue = td.textContent || td.innerText;
+//       if (txtValue.toUpperCase().indexOf(filter) > -1) {
+//         tr[i].style.display = "";
+//       } else {
+//         tr[i].style.display = "none";
+//       }
+//     }
+//   }
+// }
+
+
+
 
 function toggle1() {
 	if (f1.style.height == "0px") {
