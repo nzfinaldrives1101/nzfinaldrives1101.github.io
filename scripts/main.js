247281d6792a9d7230897786ad14b200dfa312b4
Calvin Hogg <146632082+nzfinaldrives1101@users.noreply.github.com>
2023-10-08 21:39:29 +1300

    update

--- scripts/main.js
+++ scripts/main.js
@@ -1,134 +1,101 @@
-var f1 = document.getElementById("f1");
-var f2 = document.getElementById("f2");
-var f3 = document.getElementById("f3");
-var f4 = document.getElementById("f4");
-var f5 = document.getElementById("f5");
-var f6 = document.getElementById("f6");
-var f7 = document.getElementById("f7");
-var f8 = document.getElementById("f8");
-f1.style.height = "0px";
-f2.style.height = "0px";
-f3.style.height = "0px";
-f4.style.height = "0px";
-f5.style.height = "0px";
-f6.style.height = "0px";
-f7.style.height = "0px";
-f8.style.height = "0px";
-
 search = document.getElementById("search");
 dropdown = document.getElementById("manufacturer");
 
+$(document).ready(function() {
+	$("#search").on("input", function() {
+		var value = $(this).val().toLowerCase();
+		$("#results p").filter(function() {
+			$(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+		});
+	});
+});
 
 function resetProductModal() {
 	search.value = '';
 	dropdown.value = 'all';
 }
 
-function manufacturerSelected(value) {
-	result = document.getElementsByClassName("result");
-	for (index = 0; index < result.length; ++index) {
-		if (value == "airman") {
-	    	result[index].style.height ="0px";
+function selectManufacturer(value) {
+	manufacturer = document.getElementsByClassName("manufacturer");
+	for (index = 0; index < manufacturer.length; ++index) {
+		if (value == "all") {
+	    	manufacturer[index].classList.remove("hidden");
 		}
-	}
-}
-
-$(document).ready(function(){
-	$("#search").on("input", function() {
-		var value = $(this).val().toLowerCase();
-		$("#results li").filter(function() {
-			$(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
-		});
-	});
-});
-
-
-
-// function search() {
-//   // Declare variables
-//   var input, filter, table, tr, td, i, txtValue;
-//   input = document.getElementById("search");
-//   filter = input.value.toUpperCase();
-//   table = document.getElementById("results");
-//   tr = table.getElementsByTagName("tr");
-//
-//   // Loop through all table rows, and hide those who don't match the search query
-//   for (i = 0; i < tr.length; i++) {
-//     td = tr[i].getElementsByTagName("td")[0];
-//     if (td) {
-//       txtValue = td.textContent || td.innerText;
-//       if (txtValue.toUpperCase().indexOf(filter) > -1) {
-//         tr[i].style.display = "";
-//       } else {
-//         tr[i].style.display = "none";
-//       }
-//     }
-//   }
-// }
-
-
-
-
-function toggle1() {
-	if (f1.style.height == "0px") {
-		f1.style.height = "auto";
-	} else {
-		f1.style.height = "0px";
-	}
-}
-
-function toggle2() {
-	if (f2.style.height == "0px") {
-		f2.style.height = "auto";
-	} else {
-		f2.style.height = "0px";
-	}
-}
-
-function toggle3() {
-	if (f3.style.height == "0px") {
-		f3.style.height = "auto";
-	} else {
-		f3.style.height = "0px";
-	}
-}
-
-function toggle4() {
-	if (f4.style.height == "0px") {
-		f4.style.height = "auto";
-	} else {
-		f4.style.height = "0px";
-	}
-}
-
-function toggle5() {
-	if (f5.style.height == "0px") {
-		f5.style.height = "auto";
-	} else {
-		f5.style.height = "0px";
-	}
-}
-
-function toggle6() {
-	if (f6.style.height == "0px") {
-		f6.style.height = "auto";
-	} else {
-		f6.style.height = "0px";
-	}
-}
-
-function toggle7() {
-	if (f7.style.height == "0px") {
-		f7.style.height = "auto";
-	} else {
-		f7.style.height = "0px";
-	}
-}
-
-function toggle8() {
-	if (f8.style.height == "0px") {
-		f8.style.height = "auto";
-	} else {
-		f8.style.height = "0px";
+		if (value == "airman") {
+	    	manufacturer[index].classList.add("hidden");
+			document.getElementById("airman").classList.remove("hidden");
+		}
+		if (value == "bobcat") {
+	    	manufacturer[index].classList.add("hidden");
+			document.getElementById("bobcat").classList.remove("hidden");
+		}
+		if (value == "case") {
+	    	manufacturer[index].classList.add("hidden");
+			document.getElementById("case").classList.remove("hidden");
+		}
+		if (value == "caterpillar") {
+	    	manufacturer[index].classList.add("hidden");
+			document.getElementById("caterpillar").classList.remove("hidden");
+		}
+		if (value == "daewoo") {
+	    	manufacturer[index].classList.add("hidden");
+			document.getElementById("daewoo").classList.remove("hidden");
+		}
+		if (value == "doosan") {
+	    	manufacturer[index].classList.add("hidden");
+			document.getElementById("doosan").classList.remove("hidden");
+		}
+		if (value == "hitachi") {
+	    	manufacturer[index].classList.add("hidden");
+			document.getElementById("hitachi").classList.remove("hidden");
+		}
+		if (value == "hyundai") {
+	    	manufacturer[index].classList.add("hidden");
+			document.getElementById("hyundai").classList.remove("hidden");
+		}
+		if (value == "ihi") {
+	    	manufacturer[index].classList.add("hidden");
+			document.getElementById("ihi").classList.remove("hidden");
+		}
+		if (value == "kobelco") {
+	    	manufacturer[index].classList.add("hidden");
+			document.getElementById("kobelco").classList.remove("hidden");
+		}
+		if (value == "komatsu") {
+	    	manufacturer[index].classList.add("hidden");
+			document.getElementById("komatsu").classList.remove("hidden");
+		}
+		if (value == "kubota") {
+	    	manufacturer[index].classList.add("hidden");
+			document.getElementById("kubota").classList.remove("hidden");
+		}
+		if (value == "newholland") {
+	    	manufacturer[index].classList.add("hidden");
+			document.getElementById("newholland").classList.remove("hidden");
+		}
+		if (value == "sumitomo") {
+	    	manufacturer[index].classList.add("hidden");
+			document.getElementById("sumitomo").classList.remove("hidden");
+		}
+		if (value == "sunward") {
+	    	manufacturer[index].classList.add("hidden");
+			document.getElementById("sunward").classList.remove("hidden");
+		}
+		if (value == "takeuchi") {
+	    	manufacturer[index].classList.add("hidden");
+			document.getElementById("takeuchi").classList.remove("hidden");
+		}
+		if (value == "volvo") {
+	    	manufacturer[index].classList.add("hidden");
+			document.getElementById("volvo").classList.remove("hidden");
+		}
+		if (value == "wackerneuson") {
+	    	manufacturer[index].classList.add("hidden");
+			document.getElementById("wackerneuson").classList.remove("hidden");
+		}
+		if (value == "yanmar") {
+	    	manufacturer[index].classList.add("hidden");
+			document.getElementById("yanmar").classList.remove("hidden");
+		}
 	}
 }
